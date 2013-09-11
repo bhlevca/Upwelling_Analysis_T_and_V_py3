856048dd81ae5eeb4908eb385977748981dccc60
bhlevca <bhlevca@localhost>
2013-09-10 23:54:36 -0400

    cosmetic fixes in stats

--- utils/stats.py
+++ utils/stats.py
@@ -44,22 +44,23 @@ def rsquared(x, y):
 
 
 def plot_regression(x, y, slope, intercept, point_labels = None, x_label = None, y_label = None, title = None, \
-                    r_value = None, p_value = None, fontsize = 20):
+                    r_value = None, p_value = None, fontsize = 20, bbox = False):
 
     print "intercept = %f, slope = %f" % (slope, intercept)
     fig, ax = plt.subplots()
-    ax.plot(x, y, 'bd')
+    ax.plot(x, y, 'bo')
 
     ax.plot(x, slope * x + intercept, 'r-')
 
-
+    xmax = numpy.max(x)
     scalex = ax.xaxis.get_view_interval()
     xbins = len(ax.xaxis.get_gridlines()) - 1
     xn_per_bin = (scalex[1] - scalex[0]) / xbins
 
     scaley = ax.yaxis.get_view_interval()
     ybins = len(ax.yaxis.get_gridlines()) - 1
-    yn_per_bin = (scaley[1] - scaley[0]) / ybins
+    ybin_sz = (scaley[1] - scaley[0])
+    yn_per_bin = ybin_sz / ybins
 
     if point_labels != None:
         dx = scalex[0] / scalex[1] + 3.5 / xn_per_bin
@@ -68,19 +69,26 @@ def plot_regression(x, y, slope, intercept, point_labels = None, x_label = None,
             ax.annotate(point_labels[i], xy = (x[i], y[i]), xycoords = 'data', xytext = (dx, dy) , textcoords = 'offset points',
                         ha = 'right', va = 'top', bbox = dict(fc = 'white', ec = 'none', alpha = 0.3))
 
-    bbox_props = dict(boxstyle = "square,pad=0.3", fc = "white", ec = "b", lw = 2)
+    if bbox:
+        bbox_props = dict(boxstyle = "square,pad=0.3", fc = "white", ec = "b", lw = 1)
+    else:
+        bbox_props = None
+
+    # transform = ax.transAxes ensures independence of text position from the plotting scale
     if r_value != None:
         text = "R$^2$=%4.2f" % r_value ** 2
-        x0 = scalex[0] + (xbins - 1.5) * xn_per_bin
-        y0 = scaley[0] + (ybins - 1) * yn_per_bin
+        x0 = scalex[0] + (1.2) * xn_per_bin
+        # x0 = scalex[0] + (xbins - 1.5) * xn_per_bin
+        y0 = scaley[0] + (ybins - 0.8) * yn_per_bin
         # y0 = scaley[0] + (2) * yn_per_bin
-        ax.text(x0, y0, text, ha = 'center', va = 'center', bbox = bbox_props)
+        ax.text(0.02, 0.95, text, ha = 'left', va = 'center', bbox = bbox_props, fontsize = 14, transform = ax.transAxes)
     if p_value != None:
         text2 = "p-value=%2.5f" % p_value
-        x0 = scalex[0] + (xbins - 1.5) * xn_per_bin
-        y0 = scaley[0] + (ybins - 1.5) * yn_per_bin
+        x0 = scalex[0] + (1.2) * xn_per_bin
+        # x0 = scalex[0] + (xbins - 1.5) * xn_per_bin
+
         # y0 = scaley[0] + (1) * yn_per_bin
-        ax.text(x0, y0, text2, ha = 'center', va = 'center', bbox = bbox_props)
+        ax.text(0.02, 0.9, text2, ha = 'left', va = 'center', bbox = bbox_props, fontsize = 14, transform = ax.transAxes)
 
     if x_label != None:
         plt.xlabel(x_label).set_fontsize(fontsize)
@@ -89,8 +97,10 @@ def plot_regression(x, y, slope, intercept, point_labels = None, x_label = None,
     if title != None:
         plt.title(title).set_fontsize(fontsize + 2)
 
+    ax.set_xlim(xmax = xmax + xmax / 10)
+
     plt.grid(True)
-    plt.show()
+    # plt.show()
 
 
 
